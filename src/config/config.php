9c8431c55fa8667e03ed5b8761f5c06995da965a
rabin karmacharya <rabin.karmachraya@olivemedia.co>
2015-12-31 16:22:46 +0545

    ciunittest files

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+* fooStack, CIUnit for CodeIgniter
+* Copyright (c) 2008-2009 Clemens Gruenberger
+* Released under the MIT license, see:
+* http://www.opensource.org/licenses/mit-license.php
+*/
+
+$config['ciu_subclass_prefix'] = 'CIU_';
+
+/* End of file config.php */
+/* Location: ./vendor/olivemedia/ciunittest/src/config/config.php */
---
+++ src/core/CIU_Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! defined('BASEPATH')) {
+	exit('No direct script access');
+}
+
+class CIU_Controller extends CI_Controller {
+	
+	public function index()
+	{
+		return;
+	}
+	
+}
+
+/* End of file CIU_Controller.php */
+/* Location: ./vendor/olivemedia/ciunittest/src/core/CIU_Controller.php */
